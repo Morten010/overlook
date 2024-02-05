b21fde5870971cbdadc21c8ce22cc8220110c526
Morten rasmussen <97233551+Morten010@users.noreply.github.com>
2024-02-05 15:25:20 +0100

    Update NewsCard.tsx

--- frontend/src/components/NewsCard.tsx
+++ frontend/src/components/NewsCard.tsx
@@ -11,7 +11,7 @@ const NewsCard: FC<NewsCardProps> = ({news}) => {
 
   return (
     <div
-    className='flex flex-col gap-2 shadow-xl relative overflow-hidden'
+    className='flex flex-col gap-2 shadow-xl relative overflow-hidden cursor-pointer'
     onMouseEnter={() => {
         setHide(false)
     }}
