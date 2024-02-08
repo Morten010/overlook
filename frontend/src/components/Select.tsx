0e0f382c4dd7e6085c8a866e5b7143dcc2c1d5e7
Morten rasmussen <97233551+Morten010@users.noreply.github.com>
2024-02-08 15:41:29 +0100

    more form

--- frontend/src/components/Select.tsx
+++ frontend/src/components/Select.tsx
@@ -16,54 +16,65 @@ interface SelectProps {
     value: string,
     title: string 
   }[]
-  
+  disabled?: boolean
 }
 
-const Select: FC<SelectProps> = ({title, options, selected, setSelected}) => {
+const Select: FC<SelectProps> = ({title, options, selected, setSelected, disabled = false}) => {
     const [open, setOpen] = useState(false);
-
+    console.log(disabled);
+    
   return (
-    <AnimatePresence>
-        <div
-        className='rounded-lg cursor-pointer select-none overflow-hidden'
-        >
-            <div
-            className='p-2 px-4 bg-white flex justify-between items-center'
-            onClick={() => setOpen(!open)}
-            >
-                {selected.title ? selected.title : title}
-                <CgChevronDown />
-            </div>
-            <motion.div
-            layout
-            className='bg-white max-h-[300px] overflow-auto scrollbar'
-            transition={{
-                duration: 0.18
-            }}
-            >
-                {open && options.map(option => (
-                    <motion.div
-                    initial={{
-                        opacity: 0
-                    }}
-                    animate={{
-                        opacity: 1
-                    }}
-                    exit={{
-                        opacity: 0.2
-                    }}
-                    transition={{
-                        delay: 0.16
-                    }}
-                    className='p-2 px-4 hover:bg-[#fafafa]'
-                    onClick={() => setSelected(option)}
-                    >
-                        {option.title}
-                    </motion.div>
-                ))}
-            </motion.div>
-        </div>
-    </AnimatePresence>
+    <>
+      {open && <div 
+      className='absolute top-0 left-0 w-full h-[calc(100vh-20px)] z-[5] '
+      onClick={() => disabled ? null : setOpen(false)}
+      />}
+      <AnimatePresence>
+          <div
+          className={`rounded-lg cursor-pointer select-none relative ${disabled ? "opacity-60 transition-opacity cursor-not-allowed" : ""}`}
+          >
+              <div
+              className={`p-2 px-4 bg-white flex justify-between items-center z-10 ${open ? "border-b rounded-t-lg": " rounded-lg"}`}
+              onClick={() => disabled ? null : setOpen(!open)}
+              >
+                  {selected.title ? selected.title : title}
+                  <CgChevronDown />
+              </div>
+              <motion.div
+              layout
+              className='bg-white max-h-[300px] overflow-auto scrollbar absolute  -translate-y-full  left-0 z-20 w-full rounded-b-lg'
+
+              transition={{
+                  duration: 0.18
+              }}
+              >
+                  {open && options.map(option => (
+                      <motion.div
+                      initial={{
+                          opacity: 0
+                      }}
+                      animate={{
+                          opacity: 1
+                      }}
+                      exit={{
+                          opacity: 0.2
+                      }}
+                      transition={{
+                          delay: 0.16
+                      }}
+                      className='p-2 px-4 hover:bg-[#fafafa]'
+                      onClick={() => {
+                        setSelected(option)
+                        setOpen(false)
+                      }}
+                      >
+                          {option.title}
+                      </motion.div>
+                  ))}
+              </motion.div>
+          </div>
+      </AnimatePresence>
+    </>
   )
 }
 
--- frontend/src/pages/Reservation.tsx
+++ frontend/src/pages/Reservation.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import { FC, useEffect, useState } from 'react'
 import Container from '../components/Container'
 import Select from '../components/Select'
 import { useQuery } from 'react-query'
@@ -13,6 +13,18 @@ const Reservation: FC<ReservationProps> = ({}) => {
         value: "",
         title: ""
     })
+    const [room, setRoom] = useState({
+        value: "",
+        title: ""
+    })
+    const [people, setPeople] = useState({
+        value: "",
+        title: ""
+    })
+    const [hotelData, setHotelData] = useState({
+        people: [] as { title: string; value: any; }[],
+        roomType: [] as { title: string; value: any; }[]
+    })
 
     const { data } = useQuery({
         queryFn: async () => {
@@ -23,13 +35,9 @@ const Reservation: FC<ReservationProps> = ({}) => {
             const url = 'http://localhost:4000/destinations';
             // fetch all cities
             const {data} = await axios.get(url);
-
-            console.log(data);
             // fetch all countries
             const countries = data.map(async (country: any) => {
                 const {data: newCountry} = await axios.get(url + "/" + country.slug)
-                console.log(newCountry);
-                
                 // fetch all cities
                 const cities = newCountry.cities.map(async (city: any) => {
                     const {data: newCity} = await axios.get(url + "/" + country.slug + "/" + city.slug)
@@ -40,26 +48,56 @@ const Reservation: FC<ReservationProps> = ({}) => {
 
                         
                         choices.push({
-                            value: "2",
+                            value: url + "/" + country.slug + "/" + city.slug + "/" + hotel.slug,
                             title: `${country.name} ${city.name} - ${newHotel.cities[0].hotels[0].title}`
                         })
                     })
                     await Promise.all(hotels)
                 })
-                await Promise.all(cities)
-                
-                
-                
+                await Promise.all(cities)  
             })
-
             await Promise.all(countries)
-            
-            
             
             return choices
         },
         queryKey: ["mash"]
     })
+
+
+    useEffect(() => {
+        if(!hotel.title) return
+
+        const getData = async () => {
+            const { data: { cities } } = await axios.get(hotel.value)
+            const c = cities[0]
+            const h = c.hotels[0]
+            const rooms = h.rooms
+            
+            setHotelData({
+                people: [],
+                roomType: rooms.map((r: any) => ({ value: r.room_id, title: r.title})),
+            })
+            if(room) {
+                const choosenRoom = rooms.find((r: any) => r.room_id === room.value)
+                const arr = Array(choosenRoom.num_persons).fill("")
+                const newArr = arr.map((_item, index) => {
+                    return {
+                        title: `${index + 1} Personer`,
+                        value: choosenRoom.room_id
+                    }
+                })                
+                
+                setHotelData({
+                    ...hotelData,
+                    people: newArr
+                })
+            }
+
+            
+        }
+        
+        getData()
+    }, [hotel, room])
 
   return (
     <Container>
@@ -73,12 +111,60 @@ const Reservation: FC<ReservationProps> = ({}) => {
         >
             Udfyld nedenstående formular for at reservere et af vores værelser.
         </p>
-        {data && <Select 
+        <Select 
         selected={hotel}
         setSelected={setHotel}
         title='Vælg destination & hotel'
-        options={data}
-        />}
+        options={data ? data : ["Loading..."]}
+        />
+        <div
+        className='grid grid-cols-2 mt-3 gap-3'
+        >
+            <Select 
+            selected={room}
+            setSelected={setRoom}
+            options={hotelData.roomType}
+            disabled={!hotel.title}
+            title='Værelse type'
+            />
+            <Select 
+            selected={people}
+            setSelected={setPeople}
+            options={hotelData.people}
+            disabled={!room.title}
+            title='Værelse type'
+            />
+        </div>
+        <h2
+        className='text-xl font-bold mt-2 mb-1'
+        >
+            Vælg prisklasse
+        </h2>
+        <label
+        className='flex gap-2 items-center'
+        >
+            <input 
+            type='radio'
+            name='priceclass'
+            value="normal"
+            />
+            <span>
+                Normal
+            </span>
+        </label>
+        <label
+        className='flex gap-2 items-center'
+        >
+            <input 
+            type='radio'
+            name='priceclass'
+            value="flex"
+            />
+            <span>
+                Flex
+            </span>
+        </label>
+        
     </Container>
   )
 }
